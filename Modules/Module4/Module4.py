713f39a67d88db682546fb43e072d4343b8610fb
CSI-Lorenzo-Reina <89158781+CSI-Lorenzo-Reina@users.noreply.github.com>
2021-09-10 14:23:42 -0400

    Module 4 exercise

---
+++ Modules/Module4/Module4.py
@@ -0,0 +1,11 @@
+print('What did you have for breakfast?')
+meal = 'Scrambled eggs and bacon.'
+print(meal)
+
+print('What did you eat during lunch?')
+meal = 'A hamburger and chips'
+print(meal)
+
+print('Did you eat dinner?')
+meal = 'Yes, I ate steak and mashed potatoes.'
+print(meal)
