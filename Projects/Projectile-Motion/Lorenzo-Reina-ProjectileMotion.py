d7a6a3aae11bb0043ce2712cddaabe9405855904
CSI-Lorenzo-Reina <89158781+CSI-Lorenzo-Reina@users.noreply.github.com>
2021-10-28 14:35:06 -0400

    yayayay

--- Projects/Projectile-Motion/Lorenzo-Reina-ProjectileMotion.py
+++ Projects/Projectile-Motion/Lorenzo-Reina-ProjectileMotion.py
@@ -1,5 +1,7 @@
+import math 
+
 gun = 'FN GL40'
-cartridge = '40x46 mm'
+ammunition = '40x46 M381 mm Grenade'
 FiringMode = 'Single'
 ProVel = 76
 BuildingN = 'The Shard'
@@ -9,13 +11,14 @@ g = 9.81
 def f(BuildingH,g):
     return ((2*BuildingH)/g)**1/2
 print(f'For the the projectile to reach the floor, it would take {f(BuildingH,g)} seconds.')
-hdisp = f(BuildingH,g)
+distance = f(BuildingH,g)
 
-def f(ProVel,hdisp):
-    return (ProVel*hdisp)
-print(f'The projectile will touch down at {f(ProVel,hdisp)} meters from where it was fired.')
+def f(ProVel,distance):
+    return (ProVel*distance)
+print(f'The projectile will touch down at {f(ProVel,distance)} meters from where it was fired.')
 
 print(f'''
-The gun chosen was the {gun}, a grenade launcher. This grenade launcher uses {cartridge} grenade ammunition, and fires in a {FiringMode}-fire mode.
+The gun chosen was the {gun}, a grenade launcher. This grenade launcher uses {ammunition} ammunition, and fires in a {FiringMode}-fire mode.
 The projectile velocity of the grenade is {ProVel} meters per second. The grenade launcher will be fired from {BuildingH} meters, on top of {BuildingN}.
 ''')
+
