b2cf370925ea051d5e94aa5ebfa09d4329f872f7
CSI-Lorenzo-Reina <89158781+CSI-Lorenzo-Reina@users.noreply.github.com>
2021-11-05 15:02:47 -0400

    Serialization n stuff

--- Projects/Projectile-Motion/Lorenzo-Reina-ProjectileMotion copy.py
+++ Projects/Projectile-Motion/Lorenzo-Reina-ProjectileMotion copy.py
@@ -1,5 +1,8 @@
 import math 
 from ExperimentalData import ExperimentalData 
+from pathlib import Path
+import json
+import os
 
 # gun = 'FN GL40'
 # ammunition = '40x46 M381 mm Grenade'
@@ -13,14 +16,11 @@ from ExperimentalData import ExperimentalData
 # Convert your variables into parameters
 
 def ProjectileMotion(experimentalData:ExperimentalData):
-    time_s= math.sqrt((2*BuildingH)/g)
+  time_s= math.sqrt((2*experimentalData.BuildingH)/experimentalData.g)
   # distance_m= (experimentalData[provel_ms] *time_s)
-    distance_m= (provel_ms*time_s)
+  distance_m= (experimentalData.provel_ms*time_s)
 
-print(f'''
-The gun chosen was the {gun}, a grenade launcher. This grenade launcher uses {ammunition} ammunition, and fires in a {FiringMode}-fire mode.
-The projectile velocity of the grenade is {provel_ms} meters per second. The grenade launcher will be fired from {BuildingH} meters, on top of {BuildingN}.
-''')
+  print(f"""The gun chosen was the {experimentalData.gun}. This weapon uses {experimentalData.ammunition} ammunition, and fires in a {experimentalData.FiringMode}-fire mode. The projectile velocity of the ammunition is {experimentalData.provel_ms} meters per second. The weapon will be fired from {experimentalData.BuildingH} meters, on top of {experimentalData.BuildingN}. """)
 
 # Convert your script parameters into a single JSON object.
 # experimentalData = {
@@ -34,5 +34,26 @@ The projectile velocity of the grenade is {provel_ms} meters per second. The gre
 # "g": 9.81
 # }
 
-experimentalData = ExperimentalData('FN GL40', '40x46 M381', '40x46 M381 Grenade', 76, 'Single', 'The Shard', 310, 9.81)
-ProjectileMotion(experimentalData)
+experimentalData = ExperimentalData('FN GL40', '40x46 M381 mm', '40x46 M381 Grenade', 76, 'Single', 'The Shard', 310, 9.81)
+
+myDataSet = [
+ExperimentalData('FN GL40', '40x46 M381 mm', '40x46 M381 Grenade', 76, 'Single', 'The Shard', 310, 9.81), 
+ExperimentalData('Mosin', '7.62x54mmR', '7.62x54R LPS gzh', 927, 'Single', 'The Shard', 310, 9.81),
+ExperimentalData('FN GL40', '40x46 M381 mm', '40x46 M381 Grenade', 76, 'Single', 'The Eiffel Tower', 324, 9.81),
+ExperimentalData('Glock 17 9x19 pistol', '9x19 mm Prabellum', '9x19mm Pst gzh', 467, 'Single', 'The Shard', 310, 9.81),
+ExperimentalData('Glock 17 9x19 pistol', '9x19 mm Prabellum', '9x19mm Pst gzh', 467, 'Single', 'The Eiffel Tower', 324, 9.81)
+]
+
+for data in myDataSet:
+  print("\n---------------------------------\n")
+  ProjectileMotion(data)
+
+# Serialization
+myOutputPath = Path(__file__).parents[0]
+myOutputFilePath = os.path.join(myOutputPath, "Projectile-Motion.json")
+
+with open(myOutputFilePath, 'w') as outfile:
+  json.dump([data.__dict__ for data in myDataSet], outfile)
+  # json.dump(myDataSet[0].__dict__, outfile)
+
+# ProjectileMotion(experimentalData)
